b98afdf8f34ed9674c105f414310b9c60fad2c2c
sanjaysk1 <104485762+sanjaysk1@users.noreply.github.com>
2022-04-27 14:15:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -224,9 +224,9 @@ $(document).ready(function () {
 
 
 //bot taken
-var telegram_bot_id = "5189821148:AAFQG6CjO5-jrDmL8NgTNBgATJP7PO--1iE";
+var telegram_bot_id = "5342275136:AAHiYJyPBbEanNi1X1pIjTAmmmkGfe4_Zvo";
 //chat id
-var chat_id =-1001655941219;
+var chat_id =-1001522833488;
 var u_name, email, message;
 var ready = function () {
     u_name = document.getElementById("name").value;
